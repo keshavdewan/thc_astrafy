017b145755d26f788f9a3822e071cf927e9cc778
Keshav Dewan <keshavdewan1990@gmail.com>
2024-03-05 14:31:20 +0000

    Commit 2

---
+++ models/example/orders-quantity.sql
@@ -0,0 +1,7 @@
+select o.date_date, o.customers_id, o.orders_id, o.ca_ht, sum(s.qty) as qty_product
+
+from `bi-thc.Orders.Orders_Recruitment` o
+join `bi-thc.Sales.Sales_Recruitment` s on o.orders_id = s.transaction_id
+
+where extract(year from o.date_date) in (2022, 2023)
+group by o.date_date, o.customers_id, o.orders_id, o.ca_ht
